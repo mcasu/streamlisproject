1c6d482f3caaa1c1fbf998e16bb94af901ff3127
Marco Casu <marco.casu@gmail.com>
2014-02-28 16:15:38 +0100

    Changed flowplayer emebedded code

--- players/flowplayer/play-live.php
+++ players/flowplayer/play-live.php
@@ -27,7 +27,8 @@ if(isset($_GET['stream_name']))
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link rel="stylesheet" href="//releases.flowplayer.org/5.4.6/skin/minimalist.css">
-    
+    <script src="../../js/jquery-1.11.0.min.js"></script>    
+    <script src="flowplayer-3.2.13.min.js"></script>    
 </head>
 <body>
 		<!-- HEADER -->
@@ -36,27 +37,41 @@ if(isset($_GET['stream_name']))
         </div>
 		<center>
 		<div class="flowplayer" id="player" style="text-align:center">
-		    <video>
-			<?php echo '<source type="video/flash">'; ?>
-		    </video>
+		    <!--<video>
+		    </video>-->
 		</div>
-
-<script src="../../js/jquery-1.11.0.min.js"></script>    
-<script src="http://releases.flowplayer.org/5.4.6/flowplayer.min.js"></script>
 
 <?php
 	
-	
-echo '<script>'.
+echo '<script>';
 
+/*
     'flowplayer.conf = {'.
 	'live: true,'.
 	'rtmp: "rtmp://54.213.120.163:1935/'.$app_name.'/'.$stream_name.'",'.
 	'ratio: 3/4,'.
 	'width: 640px,'.
 	'height: 480px,'.
-	'swf: "http://releases.flowplayer.org/5.4.3/flowplayer.swf" };'.
-    '</script>';
+	'swf: "http://releases.flowplayer.org/5.4.3/flowplayer.swf" };';
+*/	
+
+echo '$f("player", "flowplayer-3.2.18.swf", {'.
+        'clip: {'.
+            'url : "'.$stream_name.'",'. /*this is the name of the stream assset in the encoder*/
+	    'live : true,'.  /*// tell flowplayer it's live*/
+	    'provider: "rtmp"'.
+	'},'.
+ 
+	'plugins: {'.
+	    'rtmp: {'.
+	    'url: "flowplayer.rtmp-3.2.13.swf",'.
+	    'netConnectionUrl: "rtmp://54.213.120.163:1935/'.$app_name.'",'.  /*this is the rest of the URL excluding the stream name that you set in the encoder*/
+	    'subscribe: true'.  /*subscribe to the stream*/
+                    '}'.
+                 '}'.
+        '});';
+
+echo '</script>';
 
 ?>
 		</center>
