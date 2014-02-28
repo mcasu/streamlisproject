4da5669e5ffd66d73b75dd183d09cc3f20105974
Marco Casu <marco.casu@gmail.com>
2014-02-28 19:44:55 +0100

    Changed flowplayer emebedded code

--- players/flowplayer/play-vod.php
+++ players/flowplayer/play-vod.php
@@ -40,8 +40,8 @@ $filename_withoutext=substr($filename, 0, strlen($filename)-4);
 	<center>
 	    <?php echo '<div class="flowplayer fixed-controls play-button no-volume no-mute" style="text-align:center" data-rtmp="rtmp://54.213.120.163:1935/vod">'; ?>
 		<video autoplay>
-			<?php /*echo '<source type="video/flash" src="'.$filename_withoutext.'.flv">'; */ ?>
-			<?php echo '<source type="video/mp4" src="'.$filename_withoutext.'.mp4">'; ?>
+			<?php echo '<source type="video/flash" src="'.$filename_withoutext.'.flv">'; ?>
+			<?php /*echo '<source type="video/mp4" src="'.$filename_withoutext.'.mp4">'; */ ?>
 	        </video>
 	    </div>		
 <?php
