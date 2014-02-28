fe3f558a20f36a167cedecfba44b479ad0a628fe
Marco Casu <marco.casu@gmail.com>
2014-02-28 17:25:01 +0100

    Changed flowplayer emebedded code

--- players/flowplayer/play-live.php
+++ players/flowplayer/play-live.php
@@ -52,7 +52,7 @@ echo '<script>';
 
     'flowplayer.conf = {'.
 	'live: true,'.
-	'rtmp: "rtmp://54.213.120.163:1935/'.$app_name.'",'.
+	'rtmp: "rtmp://54.213.120.163:1935/'.$app_name.'/'.$stream_name.'",'.
 	'width: 640px,'.
 	'height: 480px,'.
 	'swf: "http://releases.flowplayer.org/5.4.3/flowplayer.swf" };';
