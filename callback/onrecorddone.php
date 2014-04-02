ffd6786d468ee7ae336a74ff60788922c9542961
Marco Casu <marco.casu@gmail.com>
2014-04-02 02:04:08 +0200

    Added script to convert recorded video to mp4 movie.

--- callback/onrecorddone.php
+++ callback/onrecorddone.php
@@ -60,6 +60,12 @@ if ($fsactions->SaveOnDemandVideoToDisk($nginx_id,$ondemand_path,$client_addr,$r
 	
 	$output = shell_exec($_SERVER['DOCUMENT_ROOT'].'/scripts/convert_video.bash '.$ondemand_hls_record_filepath.$stream_name.' '.$ondemand_filename.' '.$ondemand_path.$stream_name."/".$ondemand_basename);
 	
+	$ondemand_hls_fullpath = $ondemand_hls_record_filepath.$stream_name."/";
+	if (!symlink($ondemand_hls_fullpath.$filename, $ondemand_hls_record_filepath.$filename))
+	{
+		error_log('Creazione del link simbolico ['.$ondemand_hls_record_filepath.$filename.'] fallita!');
+	}
+	
 	if (!$dbactions->OnRecordDone($app_name,$stream_name,$ondemand_path.$stream_name."/",$ondemand_basename,$movie))
 	{
 		error_log("Recording the stream ".$stream_name." FAILED! ".$dbactions->GetErrorMessage());
