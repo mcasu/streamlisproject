15f6c88d71fad28f44b7b716881ec583892b3b8f
Marco Casu <marco.casu@gmail.com>
2014-04-02 15:32:07 +0200

    Added code to use Nginx pseudo-streaming for Apple devices.

--- admin/events_ondemand.php
+++ admin/events_ondemand.php
@@ -175,7 +175,7 @@ try
 						$ondemand_movie_bitrate=number_format($row['ondemand_movie_bitrate'],0,',','.') . " Kbps";
 						$ondemand_movie_codec=$row['ondemand_movie_codec'];
 					
-						$ondemand_hls_filename = basename($ondemand_filename);
+						$ondemand_mp4_filename = basename($ondemand_filename,".flv").".mp4";
 					
 						$thumbnail_img = '../images/thumbnails/'.basename($ondemand_filename,".flv").'.jpg';
 						
@@ -230,7 +230,7 @@ try
 							
 							echo '<li>';    
 							    echo '<div class="player_iphone">';
-								echo '<a class="play-button" href="../players/flowplayer/play-vod-html5.php?stream_name='.$ondemand_publish_code.'&filename='.$ondemand_hls_filename.'" target="_blank">'.
+								echo '<a class="play-button" href="/mp4/'.$ondemand_mp4_filename.'" target="_blank">'.
 								'<img src="../images/os_apple.png"/></a>';
 								echo '<br/>';
 								echo "<label>Guarda il video con Apple Iphone</label>";
