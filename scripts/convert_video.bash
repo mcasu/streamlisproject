5f79fe6216bb6578c1f132bc98d424406d6555a9
Marco Casu <marco.casu@gmail.com>
2014-04-03 11:04:20 +0200

    Added some debug info to video conversion process.

--- scripts/convert_video.bash
+++ scripts/convert_video.bash
@@ -5,15 +5,15 @@ BASENAME=$2
 PATH=$3
 LOGFILE="/var/log/nginx/ffmpeg-$BASENAME.log"
 
-TODAY=$(date +"%Y%m%d %H%M%S")
+TODAY=$(/bin/date +"%Y%m%d-%H:%M:%S")
 
-echo "$TODAY *** Start conversion ***" > $LOGFILE
+echo "$TODAY - *** Start conversion ***" > $LOGFILE
 echo "$TODAY - Convert file $PATH to $DIRNAME/$BASENAME.mp4" >> $LOGFILE
-time /usr/bin/avconv -y -i $PATH -vcodec libx264 $DIRNAME/$BASENAME.mp4 >>$LOGFILE 2>&1
+time /usr/bin/avconv -y -v warning -i $PATH -vcodec libx264 $DIRNAME/$BASENAME.mp4 >>$LOGFILE 2>&1
 
 RESULT=$?
 
-TODAY=$(date +"%Y%m%d %H%M%S")
+TODAY=$(/bin/date +"%Y%m%d-%H:%M:%S")
 if [ $RESULT -ne 0 ]; then
     echo "$TODAY - ERROR - File $DIRNAME/$BASENAME.mp4 conversion FAILED!" >> $LOGFILE
 else
