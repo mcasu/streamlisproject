0ebab438c9885d97755498af0697ce3db4bd6611
Marco Casu <marco.casu@gmail.com>
2014-04-03 12:29:42 +0200

    Added html5 player for HLS.

---
+++ players/html5/play-live.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<?PHP
+require_once("../../include/config.php");
+
+$utils = $mainactions->GetUtilsInstance();
+$dbactions = $mainactions->GetDBActionsInstance();
+
+if(!$mainactions->CheckLogin())
+{
+    $utils->RedirectToURL("../../login.php");
+    exit;
+}
+
+
+if(isset($_GET['app_name'])) 
+{
+	$app_name=$_GET['app_name'];
+}
+
+if(isset($_GET['stream_name'])) 
+{
+	$stream_name=$_GET['stream_name'];
+}
+
+if ($myhostname == "lnxstreamserver-dev")
+{
+    $ip_actual = $ip_private;
+}
+else
+{
+    $ip_actual = $ip_public;
+}
+
+?>
+
+<html> 
+<head> 
+    <title>HTTP Live Streaming</title> 
+</head> 
+<body>
+    <?php
+	echo '<video controls src="http://www.jwstream.org/'.$app_name.'/'.$stream_name.'.m3u8"  height="240" width="320" >';
+	echo '</video>';
+    ?>
+</body> 
+</html>
