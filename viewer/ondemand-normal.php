85dcf232f7a8c07c4f14ff3a686c090cc3ec9ffc
Marco Casu <marco.casu@gmail.com>
2014-08-24 23:35:07 +0200

    Added video element title also to viewer ondemand page.

--- viewer/ondemand-normal.php
+++ viewer/ondemand-normal.php
@@ -14,13 +14,14 @@ if(!$mainactions->CheckLogin())
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
-      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
-      <title>JW LIS Streaming - On-demand</title>
-      <link rel="STYLESHEET" type="text/css" href="../style/fg_membersite.css">
-	<link rel="STYLESHEET" type="text/css" href="../style/admin.css">
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+    <title>JW LIS Streaming - On-demand</title>
+    <link rel="STYLESHEET" type="text/css" href="../style/fg_membersite.css">
+    <link rel="STYLESHEET" type="text/css" href="../style/admin.css">
 
-	<script type="text/javascript" src="../js/jquery-1.8.3.min.js"></script>
-	<script type="text/javascript" src="../include/session.js"></script>
+    <script type="text/javascript" src="../js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="../include/session.js"></script>
+    <script type="text/javascript" src="../include/functions.js"></script>
         <script type="text/javascript">
 	
 		$(document).ready(function(){
@@ -48,6 +49,9 @@ if(!$mainactions->CheckLogin())
 		    {
 			    $(this).next(".toggle_container").slideToggle("slow");
 		    });
+		    
+		    var auto_refresh = setInterval(OndemandMp4Loading, 10000);
+		    
 		});
 	</script>
 </head>
