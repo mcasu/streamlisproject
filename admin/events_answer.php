bf6b5b0a6a72b5e832d44ef9a1c80a64316e7bab
Marco Casu <marco.casu@gmail.com>
2014-08-29 08:35:48 +0200

    Added Bistri Javascript API to create video conference.

--- admin/events_answer.php
+++ admin/events_answer.php
@@ -23,16 +23,14 @@ if (!$user_role || $user_role!="1")
 <head>
     <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
     <title>Risposte</title>
-    <!--<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">-->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
     
     <link rel="STYLESHEET" type="text/css" href="../style/fg_membersite.css">
     <link rel='stylesheet' type='text/css' href='../style/admin.css' />
 
-<!--    
     <script type="text/javascript" src="https://api.bistri.com/bistri.conference.min.js"></script>
-    <script type="text/javascript" src="../js/bistri/conference.js"></script>-->
+    <script type="text/javascript" src="../js/bistri/conference.js"></script>
 
-    <script src="//www.rtcmulticonnection.org/latest.js"></script>
 
 
     <style>
@@ -56,34 +54,6 @@ if (!$user_role || $user_role!="1")
 <br/>
 
 
-<button id="openNewSessionButton">Open New Room</button>
-
-<script>
-
-var connection = new RTCMultiConnection();
-
-// easiest way to customize what you need!
-connection.session = {
-    audio: true,
-    video: true
-};
-
-// on getting local or remote media stream
-connection.onstream = function(e) {
-    document.body.appendChild(e.mediaElement);
-};
-
-// setup signaling channel
-connection.connect();
-
-// open new session
-document.querySelector('#openNewSessionButton').onclick = function() {
-    connection.open();
-};
-
-</script>
-
-<!--
 <div class="container">
     <div class="pane" id="pane_0">
         <img src="http://static.tumblr.com/uzwqx7a/8VIm8jofz/logo.png">
@@ -99,7 +69,7 @@ document.querySelector('#openNewSessionButton').onclick = function() {
 	<input type="button" value="Quit Conference Room" id="quit" class="btn btn-danger btn-default btn-block">
     </div>
 
-</div>-->
+</div>
 
 </body>
 </html>
