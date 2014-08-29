cb63c8f70533c6ff2e310b9106b647f9fe1f2132
Marco Casu <marco.casu@gmail.com>
2014-08-29 10:06:17 +0200

    Addes vline php example application.

---
+++ vline/classes/Vline.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * JSON Web Token implementation
+ *
+ * Simple implementation of the vLine authentication process
+ *
+ */
+
+class Vline{
+	private $serviceID = "jwstream";
+	private $apiSecret = "oo-noqkigMm8aoHFsa0O_rX3ckb6zSHQ-LveR7qHM2c";
+	private $userID;
+	private $userDisplayName;
+	private $jwt;
+	
+	
+	/**
+     * @param string      $userID    The user ID
+     *
+     * @return void
+     */
+	function setUser($userID, $userDisplayName){
+		$this->userID = $userID;	
+		$this->userDisplayName = $userDisplayName;
+	}
+	
+	
+	/**
+     * @return void 
+     */
+	function init(){
+		$expiry = 48 * 60 * 60;
+		$sub = $this->serviceID. ":" . $this->userID;
+		$exp = time() + $expiry;
+		$apisecret = $this->apiSecret;
+		$apiSecretKey = JWT::urlsafeB64Decode($apisecret);
+
+		$payload = array(
+			"sub" => $sub,
+			"iss" => $this->serviceID,
+			"exp" => $exp
+		);
+		
+		$this->jwt = JWT::encode($payload, $apiSecretKey);	
+	}
+	
+	
+	/**
+     * @return jwt  
+     */
+	function getJWT(){
+		return $this->jwt;	
+	}
+	
+	
+	/**
+     * @return serviceID  
+     */
+	function getServiceID(){
+		return $this->serviceID;	
+	}
+
+	/**
+     * @return apiSecretKey  
+     */
+	function getApiSecretKey(){
+		return $this->apiSecretKey;	
+	}
+	
+	/**
+     * @return user ID
+     */
+	function getUserID(){
+		return $this->userID;
+	}
+	
+	
+	/**
+     * @return user display name
+     */
+	function getUserDisplayName(){
+		return $this->userDisplayName;
+	}
+}
+?>
