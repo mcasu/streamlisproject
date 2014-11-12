8f399c2db9145454e5578f4918ab3f4e5f1250f9
marco.casu <marco.casu@192.168.58.1>
2014-11-12 01:58:38 +0100

    Changed header from nav to navbar.

--- admin/header.php
+++ admin/header.php
@@ -22,7 +22,7 @@ if (!$user_role || $user_role!="1")
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <button type="button" class="navbar-toggle collapsed pull-left" style="margin-left: 4px;" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
