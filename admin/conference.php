0884a871137df073b5a65f743db45fb86228d0ec
marco.casu <marco.casu@192.168.58.1>
2014-11-22 00:36:12 +0100

    Refactoring video panels.

--- admin/conference.php
+++ admin/conference.php
@@ -94,17 +94,20 @@
            <div class="panel-heading pull-left">
                <h4 class="panel-title">Mio video</h4>
            </div>
-           <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
-           <div id="localStreams" class="row">
-             <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h5 class="panel-title pull-left"><b><?php $mainactions->UserFullName(); ?></b></h5>
-                </div>
-                <div class="col-xs-6 col-md-4" id="myvideo" style="width:320px;height:240px"></div>
-             </div>
-               
-             <div class="col-xs-6 col-md-4"></div>
-             <div class="col-xs-6 col-md-4"></div>
+           
+           <div class="panel-body">
+            <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+            <div id="localStreams" class="row">
+              <div class="panel panel-primary">
+                 <div class="panel-heading">
+                     <h5 class="panel-title"><b><?php $mainactions->UserFullName(); ?></b></h5>
+                 </div>
+                 <div class="col-xs-6 col-md-4" id="myvideo"></div>
+              </div>
+
+              <div class="col-xs-6 col-md-4"></div>
+              <div class="col-xs-6 col-md-4"></div>
+            </div>
            </div>
        </div>
 
@@ -113,15 +116,17 @@
                <h4 class="panel-title">Altri fratelli</h4>
            </div>
 
-           <!-- Columns are always 50% wide, on mobile and desktop -->
-           <div class="row">
-             <div class="panel panel-default">
-                <div id="video-1" class="col-xs-6 remoteStreams"></div>
-             </div>
-               
-             <div class="panel panel-default">
-                <div id="video-2" class="col-xs-6 remoteStreams"></div>
-             </div>
+           <div class="panel-body">
+            <!-- Columns are always 50% wide, on mobile and desktop -->
+            <div class="row">
+              <div class="panel panel-default">
+                 <div id="video-1" class="col-xs-6 remoteStreams"></div>
+              </div>
+
+              <div class="panel panel-default">
+                 <div id="video-2" class="col-xs-6 remoteStreams"></div>
+              </div>
+            </div>
            </div>
        </div>
      
