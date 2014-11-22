56b0657d889bac95bd2d4567b81b3b2632a7eb55
marco.casu <marco.casu@192.168.58.1>
2014-11-22 01:21:05 +0100

    Refactoring video panels.

--- admin/conference.php
+++ admin/conference.php
@@ -100,7 +100,7 @@
                 <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
                 <div id="localStreams" class="row">
                   <div class="panel panel-default">
-                     <h5 class="panel-title pull-left" style="margin-left: 2px;margin-top: 2px;"><b><?php $mainactions->UserFullName(); ?></b></h5>
+                     <h5 class="panel-title pull-center" style="margin-left: 2px;margin-top: 2px;"><b><?php $mainactions->UserFullName(); ?></b></h5>
                      <div class="col-xs-6 col-md-4" id="myvideo"></div>
                   </div>
 
