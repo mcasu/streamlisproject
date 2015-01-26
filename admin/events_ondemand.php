05f659e9957cfcd1cd3d113bb685ea656959c39a
marco.casu <marco.casu@192.168.58.1>
2015-01-26 20:47:45 +0100

    Changed ondemand video list into checked-list-box type.

--- admin/events_ondemand.php
+++ admin/events_ondemand.php
@@ -157,9 +157,9 @@ try
 
         echo '<div class="panel-heading">';
             echo '<h3 style="display:inline; vertical-align:middle; margin-right:20px">ELENCO EVENTI ON-DEMAND</h3>';
-            echo '<div class="pull-right btn-actions">';
-                echo '<button type="button" class="btn btn-danger" style="display:inline; vertical-align:middle; margin-right:4px" id="btn_video_delete">Elimina video</button>';
-                echo '<button type="button" class="btn btn-primary" style="display:inline; vertical-align:middle;" id="btn_video_archive">Archivia video</button>';
+            echo '<div class="pull-right btn-actions" style="vertical-align:middle;">';
+                echo '<button type="button" class="btn btn-danger" style="display:inline; margin-right:4px;" id="btn_video_delete">Elimina video</button>';
+                echo '<button type="button" class="btn btn-primary" style="display:inline;" id="btn_video_archive">Archivia video</button>';
             echo '</div>';
         echo '</div>';
         
@@ -366,9 +366,9 @@ try
         echo '</div>';
         
         echo '<div class="panel-footer">';
-            echo '<div class="pull-right btn-actions">';
-                echo '<button type="button" class="btn btn-danger" style="display:inline; vertical-align:middle; margin-right:4px" id="btn_video_delete">Elimina video</button>';
-                echo '<button type="button" class="btn btn-primary" style="display:inline; vertical-align:middle;" id="btn_video_archive">Archivia video</button>';
+            echo '<div class="pull-right btn-actions" style="vertical-align:middle;">';
+                echo '<button type="button" class="btn btn-danger" style="display:inline; margin-right:4px;" id="btn_video_delete">Elimina video</button>';
+                echo '<button type="button" class="btn btn-primary" style="display:inline;" id="btn_video_archive">Archivia video</button>';
             echo '</div>';
         echo '</div>';
         
