d61013326691c35112cb702fecabfe99f9d25604
marco.casu <marco.casu@192.168.58.1>
2015-01-26 17:33:23 +0100

    Changed ondemand video list into checked-list-box type.

--- admin/events_ondemand.php
+++ admin/events_ondemand.php
@@ -69,8 +69,8 @@ $(document).ready(function()
     
 		function(data,status)
 		{
-		    alert("Data: " + data + "\nStatus: " + status + "\nVideo element: " + $(this).find(".video_element").first().attr('class'));
-                    var ul_video_obj = $(this).find(".video_element").first();
+		    alert("Data: " + data + "\nStatus: " + status + "\nVideo element: " + $(this).children(".video_element").attr('class'));
+                    var ul_video_obj = $(this).children(".video_element");
                     ul_video_obj.fadeOut(1000, function()
                     {
                         var video_title_obj = null;
