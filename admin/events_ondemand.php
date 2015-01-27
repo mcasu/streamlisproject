2b0420595034d0e37fe72baa140b679cd2800a70
marco.casu <marco.casu@192.168.58.1>
2015-01-27 23:56:32 +0100

    Pull-right on delete icon.

--- admin/events_ondemand.php
+++ admin/events_ondemand.php
@@ -259,6 +259,13 @@ try
 
                                                         echo '<li class="list-group-item video_list_element">';
 
+                                                            echo '<div id="'.$ondemand_id.'" class="pull-right video_delete">';
+                                                                echo '<a class="event_ondemand_delete">'.
+                                                                '<span class="glyphicon glyphicon-trash"></span>'.
+                                                                //'<img src="../images/delete.png"/>'.
+                                                                '</a>';
+                                                            echo '</div>';
+                                                            
                                                             echo '<div class="video_element_title" id="'.$ondemand_id.'" style="display:inline; margin-left:8px;">';
                                                             
                                                                 //echo '<input type="checkbox" id="checkbox" style="display:inline;"/>';
@@ -284,7 +291,7 @@ try
 
                                                             echo '<ul class="video_element">';
 
-                                                                echo '<li pull-right>';
+                                                                echo '<li>';
                                                                     echo '<div id="'.$ondemand_id.'" class="video_delete">';
                                                                         echo '<a class="event_ondemand_delete">'.
                                                                         '<img src="../images/delete.png"/></a>';
