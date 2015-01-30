6c001a9b0dd6f2e6bd8911075bb0462561cd0b67
marco.casu <marco.casu@192.168.58.1>
2015-01-30 17:57:11 +0100

    Refactoring events_ondemand.php page style.

--- admin/events_ondemand.php
+++ admin/events_ondemand.php
@@ -38,6 +38,7 @@ $(document).ready(function()
     
     $(".video_checkbox").change(function()
     {
+        alert("Selezionato...");
         if($(this).attr("checked"))
         {
             //call the function to be fired
