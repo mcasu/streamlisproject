5d09ccbc6193bb45aeb52bc7d6a38abd76f4d79c
marco.casu <marco.casu@192.168.58.1>
2015-02-02 01:59:36 +0100

    Added players number info to the events_live.php page.

--- include/dbactions.php
+++ include/dbactions.php
@@ -1088,10 +1088,12 @@ class DBActions
             'where e1.stream_name = \''.$stream_name.'\' and '.
             'e1.event_date = \''.$date_now.'\' and '.
             'e1.event_call like \'%play%\' and '.
+            'e1.app_name not like \'%vod%\' and '.
             'e1.client_addr not like \'%unix%\' and '.
             'e1.client_addr not like \'%127.0.0.1%\' '.
             'and e1.event_time = '.
-            '(select max(e2.event_time) from events e2 where e2.stream_name = e1.stream_name and e2.event_date = e1.event_date and e2.event_call = e1.event_call and e2.client_addr = e1.client_addr) '.
+            '(select max(e2.event_time) from events e2 '.
+            'where e2.stream_name = e1.stream_name and e2.event_date = e1.event_date and e2.event_call = e1.event_call and e2.app_name = e1.app_name and e2.client_addr = e1.client_addr) '.
             'order by client_addr, nginx_id';
             
             /*
