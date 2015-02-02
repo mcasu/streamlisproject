8dd109ad6180386c9642d6ccfce57aae7291ab76
marco.casu <marco.casu@192.168.58.1>
2015-02-02 03:16:50 +0100

    Added players number info to the events_live.php page.

--- include/functions.php
+++ include/functions.php
@@ -110,13 +110,17 @@ function PlayDoneEventFound($player_events, $event)
     $play_done_found = false;
     foreach ($player_events as $pe) 
     {
-        //error_log("EVENTO [" .$pe['event_call']. "] - " . $pe['event_time'] . " - " .$pe['nginx_id']);
+        error_log("EVENTO [" .$pe['event_call']. "] - " . $pe['event_time'] . " - " .$pe['nginx_id']);
        
+        $date_event_play_done = new DateTime($pe['event_date'] . ' ' . $pe['event_time']);
+        $date_event_play = new DateTime($event['event_date'] . ' ' . $event['event_time']);
+        
         if ($pe['nginx_id'] === $event['nginx_id'] && 
                 $pe['event_call'] === "play_done" &&
-                $pe['client_addr'] === $event['client_addr'])
+                $pe['client_addr'] === $event['client_addr'] &&
+                $date_event_play_done > $date_event_play)
         {
-            //error_log("PLAY DONE FOUND: " . $pe['event_time'] . " - " .$pe['nginx_id'] . " - " .$pe['event_call']);
+            error_log("PLAY DONE FOUND: " . $pe['event_time'] . " - " .$pe['nginx_id'] . " - " .$pe['event_call']);
             $play_done_found = true;
             break;
         }
