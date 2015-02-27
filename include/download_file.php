23fe9a9fa0139235eac8baf79a41fdc9d6e71b6b
marco.casu <marco.casu@NEWLIFE>
2015-02-27 23:29:08 +0100

    Added download_file.php

--- include/download_file.php
+++ include/download_file.php
@@ -6,8 +6,23 @@
  * and open the template in the editor.
  */
 
+ini_set("allow_url_fopen", true);
+
 $file_url = filter_input(INPUT_GET, 'file_url');
 
 header('Content-disposition: attachment; filename=adunanza.mp4');
+header("Content-Type: application/force-download");
+header("Content-Type: application/download");
 header('Content-type: video/mp4');
-readfile($file_url);
+header("Content-Length: " . filesize($file_url));
+
+
+$fp = fopen($file_url, "r"); 
+while (!feof($fp))
+{
+    echo fread($fp, 65536); 
+    flush(); // this is essential for large downloads
+}  
+fclose($fp);
+
+//readfile($file_url);
