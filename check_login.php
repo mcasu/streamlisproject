441f56a44f710b72e282d95be35b37c073f3d4b9
marco.casu <marco.casu@NEWLIFE>
2015-03-04 12:45:48 +0100

    Added publisher section.

--- check_login.php
+++ check_login.php
@@ -10,8 +10,8 @@ if(!$mainactions->CheckLogin())
     exit;
 }
 
-$user_role = $mainactions->GetSessionUserRole();
-if (empty($user_role) || $user_role != "1" || $user_role != "2" || $user_role != "3")
-{
-    $utils->RedirectToURL("/login.php");
-}
+//$user_role = $mainactions->GetSessionUserRole();
+//if (empty($user_role) || $user_role != "1" || $user_role != "2" || $user_role != "3")
+//{
+//    $utils->RedirectToURL("/login.php");
+//}
