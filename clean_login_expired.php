58228bcc4d40c5224bd3a33776df6a6f4fb8db91
marco.casu <marco.casu@NEWLIFE>
2015-03-19 00:24:11 +0100

    Fixing clean login expired function.

--- clean_login_expired.php
+++ clean_login_expired.php
@@ -20,7 +20,7 @@ else
 
 $result = $dbactions->CleanLoginOlderThan($seconds);
 
-if ( ((int)$result) == -1 || $result == false)
+if (((int)$result) == -1)
 {
     echo date("Y-m-d H:i:s") . " - FAILED - Pulito [".$result."] utenti.\n" . $mainactions->GetErrorMessage() . "\n" . $dbactions->GetErrorMessage()."\n";
 }
