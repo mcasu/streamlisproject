57b04baab0f5718b992e2259bdcbd810b645bb64
marco.casu <marco.casu@NEWLIFE>
2015-06-01 11:06:55 +0200

    Aggiunto il menù Operazioni per il publisher.

--- publisher/actions.php
+++ publisher/actions.php
@@ -21,15 +21,6 @@
     
 $(document).ready(function()
 {
-
-    $('#ondemand_actions_join_table').DataTable({
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/f2c75b7247b/i18n/Italian.json"
-        },
-        "aoColumnDefs": [{ "bSortable": false, "aTargets": [ 0 ] }],
-        "order": [[ 2, 'asc' ]]
-    });
-    
     $('#ondemand_actions_join_table').load("/include/functions.php?fname=get_ondemand_actions_join");
     
 });
@@ -70,13 +61,13 @@ $(document).ready(function()
 {
 //    $('#ondemand_actions_join_table').load("/include/functions.php?fname=get_ondemand_actions_join");
     
-//    $('#ondemand_actions_join_table').DataTable({
-//        "language": {
-//            "url": "//cdn.datatables.net/plug-ins/f2c75b7247b/i18n/Italian.json"
-//        },
-//        "aoColumnDefs": [{ "bSortable": false, "aTargets": [ 0 ] }],
-//        "order": [[ 2, 'asc' ]]
-//    });
+    $('#ondemand_actions_join_table').DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/f2c75b7247b/i18n/Italian.json"
+        },
+        "aoColumnDefs": [{ "bSortable": false, "aTargets": [ 0 ] }],
+        "order": [[ 2, 'asc' ]]
+    });
     
 });
 
