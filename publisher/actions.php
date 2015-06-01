87b17d1a7c0aea330dc404b22f940f01f528d78d
marco.casu <marco.casu@NEWLIFE>
2015-06-01 11:15:43 +0200

    Aggiunto il menù Operazioni per il publisher.

--- publisher/actions.php
+++ publisher/actions.php
@@ -15,18 +15,6 @@
     <script type="text/javascript" src="../js/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="../include/session.js"></script>
     <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/jquery.dataTables.min.js"></script>    
-    
-<script type="text/javascript">
-    
-$(document).ready(function()
-{
-    $('#ondemand_actions_join_table').load("/include/functions.php?fname=get_ondemand_actions_join");
-    
-});
-
-</script>
-    
 </head>
 
 
@@ -40,14 +28,14 @@ $(document).ready(function()
 
         <div class="panel-body">
             <table class="table table-hover" id="ondemand_actions_join_table">
-                <thead>
+<!--                <thead>
                     <tr class="head">
                         <th></th>
                         <th>ID OPERAZIONE</th>
                         <th>ONDEMAND VIDEO DA UNIRE</th>
                         <th>STATO OPERAZIONE</th>
                     </tr>
-                </thead>
+                </thead>-->
                 
                 <!--<tbody></tbody>-->
             </table>
@@ -55,11 +43,12 @@ $(document).ready(function()
     </div>
 </div>
 
+<script src="../js/jquery.dataTables.min.js"></script> 
 <script type="text/javascript">
     
 $(document).ready(function()
 {
-//    $('#ondemand_actions_join_table').load("/include/functions.php?fname=get_ondemand_actions_join");
+    $('#ondemand_actions_join_table').load("/include/functions.php?fname=get_ondemand_actions_join");
     
     $('#ondemand_actions_join_table').DataTable({
         "language": {
