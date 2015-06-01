692385a62b853a0e39b322ff309d3b6f7f1c3c34
marco.casu <marco.casu@NEWLIFE>
2015-06-01 10:27:01 +0200

    Aggiunto il menù Operazioni per il publisher.

--- publisher/actions.php
+++ publisher/actions.php
@@ -21,16 +21,15 @@
     
 $(document).ready(function()
 {
-
+    $('#ondemand_actions_join_table').load("/include/functions.php?fname=get_ondemand_actions_join");
+    
     $('#ondemand_actions_join_table').DataTable({
         "language": {
             "url": "//cdn.datatables.net/plug-ins/f2c75b7247b/i18n/Italian.json"
         },
         "aoColumnDefs": [{ "bSortable": false, "aTargets": [ 0 ] }],
-        "order": [[ 2, 'asc' ]]
+        "order": [[ 1, 'asc' ]]
     });
-    
-    $('#ondemand_actions_join_table').load("/include/functions.php?fname=get_ondemand_actions_join");
     
 });
 
