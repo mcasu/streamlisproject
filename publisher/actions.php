655e94f1608998571c61f0595df2aebced5b92d1
marco.casu <marco.casu@NEWLIFE>
2015-06-03 13:01:41 +0200

    Cerco di risolvere il problema della cancellazione operazioni selezionate.

--- publisher/actions.php
+++ publisher/actions.php
@@ -100,7 +100,7 @@ $(document).ready(function()
         console.log("Numero record selezionati: " + joinTable.rows('.selected').data().length);
         
         var joinSelectedIds = [];
-        joinTable.row('.selected').column(0).data().each( function (value, index ) 
+        joinTable.row('.selected').data().column(0).data().each( function (value, index ) 
         {
             console.log( 'Counter: ' + index + ' Data: ' + value);
             
