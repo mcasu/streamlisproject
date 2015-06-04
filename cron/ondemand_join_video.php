e8b91faf7510d90446676032714a26c8535379d1
marco.casu <marco.casu@NEWLIFE>
2015-06-05 01:19:00 +0200

    Aggiunto script php da lanciare in cron per unire i video.

--- cron/ondemand_join_video.php
+++ cron/ondemand_join_video.php
@@ -58,21 +58,62 @@ while($row = mysql_fetch_array($actionsJoin))
         file_put_contents($ondemandActionFilename, $bashInitHead, LOCK_EX);
 
         $mkfifoCommandLine = "\n\n";
+        $catCommandLine = "cat ";
         for ($i = 1; $i <= $videoToJoinNumber ; $i++) 
         {
-            $mkfifoCommandLine .= "/usr/bin/mkfifo " . $ondemand_actions_path . "fifo-" . $row['ondemand_actions_join_id'] . "-" . $i .".v\n";
+            $fifoFilename = $ondemand_actions_path . "fifo-" . $row['ondemand_actions_join_id'] . "-" . $i .".v\n";
+            
+            $mkfifoCommandLine .= "/usr/bin/mkfifo " . $fifoFilename;
+            $catCommandLine .= $fifoFilename . " ";
         }
         $mkfifoCommandLine .= "/usr/bin/mkfifo " . $ondemand_actions_path . "fifo-" . $row['ondemand_actions_join_id'] . "-all.v\n";
-
+        
+        $fifoFilenameAll = $ondemand_actions_path . "fifo-" . $row['ondemand_actions_join_id'] . "-all.v";
+        $catCommandLine .= "> " . $fifoFilenameAll . " & ";
+        
         file_put_contents($ondemandActionFilename, $mkfifoCommandLine, FILE_APPEND | LOCK_EX);
-
-        $avconvCommandLine = '';
-
-
+        file_put_contents($ondemandActionFilename, "\n\n", FILE_APPEND | LOCK_EX);
+        
+        $count = 1;
+        while($ondemandVideo = mysql_fetch_array($ondemandVideoInfos))
+        {
+            $videoFilenameSrc = $ondemandVideo['ondemand_path'] . $ondemandVideo['ondemand_filename'];
+            $videoFilenameDst = $ondemand_actions_path . $ondemandVideo['ondemand_filename'];
+            copy($videoFilenameSrc, $videoFilenameDst);
+            
+            $fifoFilename = $ondemand_actions_path . "fifo-" . $row['ondemand_actions_join_id'] . "-" . $count .".v";
+            
+            if ($count == 1)
+            {
+                $avconvCommandLineInit .= '/usr/bin/avconv -i ' . $videoFilenameDst . ' -an -f yuv4mpegpipe - > ' . $fifoFilename .' < /dev/null & ';
+            }
+            else
+            {
+                $avconvCommandLineInit .= '{ /usr/bin/avconv -i ' . $videoFilenameDst . ' -an -f yuv4mpegpipe - < /dev/null | tail -n +2 > ' . $fifoFilename . ' ; } & ';
+            }
+            
+            $count++;
+        }
+        
+        $avconvCommandLineFin = '/usr/bin/avconv -f yuv4mpegpipe -i ' . $fifoFilenameAll . ' -vcodec libx264 -profile:v main -y ' . $row['ondemand_actions_join_id'] . '-all.flv';
+        
+        $avconvCommandLine = $avconvCommandLineInit . $catCommandLine . $avconvCommandLineFin . " > /var/log/nginx/" . $row['ondemand_actions_join_id'] . ".log 2>&1";
+        
+//        $avconvCommandLine = 'avconv -i video01.flv -an -f yuv4mpegpipe - > temp01.v < /dev/null & '.
+//                '{ avconv -i video02.flv -an -f yuv4mpegpipe - < /dev/null | tail -n +2 > temp02.v ; } & '.
+//                '{ avconv -i video03.flv -an -f yuv4mpegpipe - < /dev/null | tail -n +2 > temp03.v ; } & '.
+//                'cat temp01.v temp02.v temp03.v > all.v & '.
+//                'avconv -f yuv4mpegpipe -i all.v -vcodec libx264 -profile:v main -y output.flv';
+        
+        
+        file_put_contents($ondemandActionFilename, $avconvCommandLine, FILE_APPEND | LOCK_EX);
+        
         if (!file_exists($ondemand_actions_path))
         {
             mkdir($ondemand_actions_path, 0755, true);
         }
+        
+        // PRIMA DI ESEGUIRE RIMUOVERE I VECCHI FILE DI LOG join_*.log
         
         echo "\nINFO - ACTION-> " . $row['ondemand_actions_join_id'] . " - COUNT-> " . $videoToJoinNumber ."\n";
     
@@ -82,7 +123,6 @@ while($row = mysql_fetch_array($actionsJoin))
         error_log("ERROR - ondemand_join_video.php - ACTIONS-> " . $row['ondemand_actions_join_id'] . " - " . $e->getMessage());
         continue;
     }
-
-    
 }
+
 
