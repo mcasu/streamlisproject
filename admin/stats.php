2bab0e1ee40e6afadb6837b105b037d3f3084d24
marco.casu <marco.casu@NEWLIFE>
2015-07-31 18:03:33 +0200

    Refactoring pagina admin dashboard.

--- admin/stats.php
+++ admin/stats.php
@@ -3,7 +3,7 @@ include("../check_login.php");
 include(getenv("DOCUMENT_ROOT") . "/include/check_role_admin.php");
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it-IT" lang="it-IT">
 <head>
     <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
