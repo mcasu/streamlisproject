3e13763c3a7f4f18cc649f0e1782bcdb7f34679d
marco.casu <marco.casu@NEWLIFE>
2016-01-12 02:44:47 +0100

    Problema: quando faccio il refresh della tabella DataTables dalla finestra modale, tutti i record vengono selezionati.

--- include/ssp.php
+++ include/ssp.php
@@ -51,7 +51,7 @@ class SSP {
                 // Is there a formatter?
                 if ( isset( $column['formatter'] ) ) {
                     $row[ $column['dt'] ] = ($isJoin) ? $column['formatter']( $data[$i][ $column['field'] ], $data[$i] ) : $column['formatter']( $data[$i][ $column['db'] ], $data[$i] );
-                    error_log("INFO - FIELD->" . $column['formatter']( $data[$i][ $column['field'] ], $data[$i]) . " [" . $data[$i][ $column[$primaryKey] ] . "]");
+                    error_log("INFO - FIELD->" . $column['formatter']( $data[$i][ $column['field'] ], $data[$i]) . " [" . $data[$i][ $primaryKey ] . "]");
                 }
                 else {
                     $row[ $column['dt'] ] = htmlentities( ($isJoin) ? $data[$i][ $columns[$j]['field'] ] : $data[$i][ $columns[$j]['db'] ] );
