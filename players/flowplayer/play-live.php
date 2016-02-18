df64f99c6c07cde9584c04f65b6cd15809cbb7b2
marco.casu <marco.casu@NEWLIFE>
2016-02-18 17:34:15 +0100

    Applicare alcuni migliorie alle finestre di video on-demand e live.

--- players/flowplayer/play-live.php
+++ players/flowplayer/play-live.php
@@ -57,19 +57,18 @@ else
 <script src="../../js/jquery-1.11.0.min.js"></script>    
 <script src="flowplayer.min.js"></script>
 
-        <div class="span-18" align="center">
+    <div class="container-fluid">
+        <center>
             <h1>Live Video Streaming</h1>
-        </div>
+        </center>
+        <?php echo '<div class="flowplayer fixed-controls play-button no-volume no-mute" style="text-align:center" '.
+            'data-rtmp="rtmp://www.streamlis.it:1935/'.$app_name.'">'; ?>
+            <video autoplay>
+                    <?php echo '<source type="video/flash" src="'.$stream_name.'">'; ?>
+            </video>
+        <?php echo '</div>' ?>
+    </div>
 	
-	<center>
-	    <?php echo '<div class="flowplayer fixed-controls play-button no-volume no-mute" style="text-align:center" '.
-			'data-rtmp="rtmp://www.streamlis.it:1935/'.$app_name.'">'; ?>
-	        <video autoplay>
-			<?php echo '<source type="video/flash" src="'.$stream_name.'">'; ?>
-	        </video>
-	    </div>
-
-   
 <?php
 	
 echo '<script>';
@@ -77,10 +76,8 @@ echo '<script>';
     'flowplayer.conf = {'.
 	'live: true,'.
 	'rtmp: "rtmp://'.$ip_actual.':1935/'.$app_name.'/'.$stream_name.'",'.
-	'ratio: 3/4,'.
+	'ratio: 9/16,'.
 	'width: 480px,'.
-	'height: 640px,'.
-	/*'swf: "http://releases.flowplayer.org/5.4.3/flowplayer.swf" */
 	'swf: "flowplayer.swf"'.
 	 '};';
 
@@ -88,6 +85,5 @@ echo '</script>';
 
 ?>
 
-		</center>
 </body>
 </html>
