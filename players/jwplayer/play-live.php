234d23069f59465d0c476646b6d3f6ec7bbd84e4
marco.casu <marco.casu@NEWLIFE>
2016-02-18 18:09:08 +0100

    Applicare alcuni migliorie alle finestre di video on-demand e live.

--- players/jwplayer/play-live.php
+++ players/jwplayer/play-live.php
@@ -56,7 +56,7 @@ else
     <div class="container-fluid">
         <center>
             <h1>Live Video Streaming</h1>
-            <div id="player" style="text-align:center; vertical-align:middle;"/>
+            <div id="player" style="text-align:center; vertical-align:middle; width: 100%; height: 90%"/>
         </center>
         
     </div>
