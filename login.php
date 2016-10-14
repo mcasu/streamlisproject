ca807f3dfa15ccc816540fab53a949526057de0d
marco.casu <marco.casu@NEWLIFE>
2016-10-14 18:32:35 +0200

    Aggiunto meta-tag per attivazione Gmail Suite.

--- login.php
+++ login.php
@@ -45,15 +45,16 @@ if($userIsLogged)
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it-IT" lang="it-IT">
 <head>
-   <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
-   <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'/>
-   <meta name="viewport" content="width=device-width, initial-scale=1"/>
-   <title>Stream LIS - Login</title>
-   <link rel="stylesheet" href="//www.streamlis.it/style/bootstrap.min.css">
-   <link rel='stylesheet' type='text/css' href='//www.streamlis.it/style/admin.css' />
-   <script type="text/javascript" src="//www.streamlis.it/js/jquery-1.11.0.min.js"></script>
-   <script type='text/javascript' src='//www.streamlis.it/js/jquery.validate.js'></script>
-   <script src="//www.streamlis.it/js/bootstrap.min.js"></script>
+    <meta name="google-site-verification" content="35f4R1fOBfkiDv-s7lKoBtDb55-bBAN__eh6gCk6xxQ" />
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Stream LIS - Login</title>
+    <link rel="stylesheet" href="//www.streamlis.it/style/bootstrap.min.css">
+    <link rel='stylesheet' type='text/css' href='//www.streamlis.it/style/admin.css' />
+    <script type="text/javascript" src="//www.streamlis.it/js/jquery-1.11.0.min.js"></script>
+    <script type='text/javascript' src='//www.streamlis.it/js/jquery.validate.js'></script>
+    <script src="//www.streamlis.it/js/bootstrap.min.js"></script>
 </head>
 
 <body>
