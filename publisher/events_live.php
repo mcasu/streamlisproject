7b847d1109169c5e74b0c65b7985f3521c7f7779
marco.casu <marco.casu@trovaprezzi.it>
2016-12-20 20:25:03 +0100

    Added buttons to view live play links.

--- publisher/events_live.php
+++ publisher/events_live.php
@@ -12,6 +12,7 @@ include(getenv("DOCUMENT_ROOT") . "/include/check_role_publisher.php");
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title>Stream LIS - Eventi live</title>
     <link rel="stylesheet" href="../style/bootstrap.min.css">
+    <link rel="stylesheet" href="../style/jquery-ui.min.css"/>
     <link rel='stylesheet' type='text/css' href='../style/admin.css' />
 
 <script type="text/javascript" src="../js/jquery-1.11.0.min.js"></script>
