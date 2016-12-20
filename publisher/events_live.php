2fcf9c1cd7f6bb23ea9a050f5e7be67d63df837b
marco.casu <marco.casu@trovaprezzi.it>
2016-12-20 20:23:30 +0100

    Added buttons to view live play links.

--- publisher/events_live.php
+++ publisher/events_live.php
@@ -15,6 +15,7 @@ include(getenv("DOCUMENT_ROOT") . "/include/check_role_publisher.php");
     <link rel='stylesheet' type='text/css' href='../style/admin.css' />
 
 <script type="text/javascript" src="../js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="../js/jquery-ui.min.js"></script>
 <script type="text/javascript" src="../include/session.js"></script>
 <script src="../js/bootstrap.min.js"></script>
 
