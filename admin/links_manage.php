fd70e6af056dfb4656234ccaddcfc166cf9ad171
marco.casu <marco.casu@NB-MCASU>
2016-12-25 21:41:40 +0100

    Cambiata la query nella pagina delle relazioni per permettere di associare anche congregazioni.

--- admin/links_manage.php
+++ admin/links_manage.php
@@ -225,11 +225,11 @@ try
 				    echo '<td>';
                                         echo '<br/>';
                                         echo '<br/>';
-					echo '<select multiple class="selectpicker group_unlinked" id="gul_'.$group_id.'" data-style="btn-primary" data-selected-text-format="count > 2" title="Seleziona i gruppi da associare..." style="min-height: 140px;">';
+					echo '<select multiple class="selectpicker form-control group_unlinked" id="gul_'.$group_id.'" data-style="btn-primary" data-selected-text-format="count > 2" title="Seleziona i gruppi da associare..." style="min-height: 140px;">';
 					while($row = mysql_fetch_array($groups_available))
 					{
 						$groupType = $row['group_role'] == 1 ? "[C] " : "[G] ";
-						echo '<option id="'.$row['group_id'].'" data-content="<span class=\'label label-default\'>'.$groupType.'</span> '.$row['group_name'].'">'.$row['group_name'].'</option>';
+						echo '<option id="'.$row['group_id'].'" data-content="<span class=\'label label-default\'>'.$groupType.'</span>  '.$row['group_name'].'"> '.$row['group_name'].'</option>';
 					}
 					echo '</select>';
 				    echo '</td>';
