661701a3348f7c0143858b23de5210cd8dacae8d
marco.casu <marco.casu@NB-MCASU>
2017-01-02 11:37:00 +0100

    Ordinare la bootstrap select dopo la modifica.

--- admin/links_manage.php
+++ admin/links_manage.php
@@ -113,16 +113,16 @@ var sortSelect = function (select, attr, order) {
     if(attr === 'text'){
         if(order === 'asc'){
             $(select).html($(select).children('option').sort(function (x, y) {
-                return $(x).text().toUpperCase() < $(y).text().toUpperCase() ? -1 : 1;
+                return $(x).text().split(' - ')[1].toUpperCase() < $(y).text().split(' - ')[1].toUpperCase() ? -1 : 1;
             }));
-            $(select).get(0).selectedIndex = 0;
+            //$(select).get(0).selectedIndex = 0;
             //e.preventDefault();
         }// end asc
         if(order === 'desc'){
             $(select).html($(select).children('option').sort(function (y, x) {
-                return $(x).text().toUpperCase() < $(y).text().toUpperCase() ? -1 : 1;
+                return $(x).text().split(' - ')[1].toUpperCase() < $(y).text().split(' - ')[1].toUpperCase() ? -1 : 1;
             }));
-            $(select).get(0).selectedIndex = 0;
+            //$(select).get(0).selectedIndex = 0;
             //e.preventDefault();
         }// end desc
     }
