3489fd9468bcaefb86703733d39ff0c31ea27578
marco.casu <marco.casu@NEWLIFE>
2017-07-19 18:24:32 +0200

    Aggiunto progress bar per lo spazio occupato dallo stream live.

--- include/functions.js
+++ include/functions.js
@@ -194,7 +194,7 @@ StreamVideoSizeUpdate = function()
                         var percent = 100 * res / 1024;
                         progressBarObj.css('width', percent+'%').attr('aria-valuenow', res);
                         console.log("ProgressBar - updated aria-valuenow to [" + res + "]");
-                        progressBarObj.text(res + ' MB (MAX 1GB)');
+                        progressBarObj.find('span').text(res + ' MB (MAX 1GB)');
 
                         if (res <= 500)
                         {
