a0ecae20d3baf8acb73bb4ad82193438b6235b46
marco.casu <marco.casu@NEWLIFE>
2017-10-14 21:55:26 +0200

    Provato a cambiare il player per desktop da flash a html5.

--- admin/events_ondemand.php
+++ admin/events_ondemand.php
@@ -224,7 +224,7 @@ $(document).ready(function()
 	var url = $(this).attr("href");
 	var windowName = "Player";//$(this).attr("name");
 	//var windowSpecs = 'width=640,height=480, scrollbars=yes, resizable=yes, status=no, toolbar=no, menubar=no, location=no';
-        var windowSpecs = 'width=600, height=400, scrollbars=yes, resizable=yes, status=no, toolbar=no, menubar=no, location=no';
+        var windowSpecs = 'width=640, height=360, scrollbars=yes, resizable=yes, status=no, toolbar=no, menubar=no, location=no';
 	
 	window.open(url, windowName, windowSpecs);
 	
