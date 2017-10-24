4e690ee807abf15044bbc2bfc859de7d2b4e8b64
marco.casu <marco.casu@NEWLIFE>
2017-10-24 22:37:00 +0200

    Aggiunta schedulazione manuale della conversione video in mp4.

--- admin/events_ondemand.php
+++ admin/events_ondemand.php
@@ -13,12 +13,15 @@ include(getenv("DOCUMENT_ROOT") . "/include/check_role_admin.php");
     <title>Stream LIS - Eventi On-Demand</title>
     
     <link rel="stylesheet" href="../style/bootstrap.min.css">
-    <link rel='stylesheet' type='text/css' href='../style/admin.css' />
+    <link rel="stylesheet" href="../style/jquery-ui.min.css"/>
+    <link rel='stylesheet' href='../style/admin.css' />
 
     <script type="text/javascript" src="../js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/jquery-ui.min.js"></script>
     <script type="text/javascript" src="../include/session.js"></script>
     <script type="text/javascript" src="../include/functions.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
+    
 
 <script type="text/javascript">
 
