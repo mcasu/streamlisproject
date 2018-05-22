423d004f8e16d40bd0067d85bfd946244eaa943e
marco.casu <marco.casu@NEWLIFE>
2018-05-22 17:36:43 +0200

    Aggiunto footer con privacy policy.

--- login.php
+++ login.php
@@ -55,6 +55,8 @@ if($userIsLogged)
     <script type="text/javascript" src="//www.streamlis.it/js/jquery-1.11.0.min.js"></script>
     <script type='text/javascript' src='//www.streamlis.it/js/jquery.validate.js'></script>
     <script src="//www.streamlis.it/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript"> var _iub = _iub || []; _iub.csConfiguration = {"lang":"it","siteId":1168862,"cookiePolicyId":74934126,"banner":{"textColor":"#fff","backgroundColor":"#333"}}; </script><script type="text/javascript" src="//cdn.iubenda.com/cookie_solution/safemode/iubenda_cs.js" charset="UTF-8" async></script> 
 </head>
 
 <body>
