095cc0a200e908fa57d463650caae74e67b6026d
marco.casu <marco.casu@NEWLIFE>
2018-05-22 17:10:50 +0200

    Aggiunto footer con privacy policy.

--- include/footer.php
+++ include/footer.php
@@ -21,4 +21,7 @@
 <div class="text-center" style="background-color: #3c78c3; position:relative; bottom:0; border-right: 2px; border-left: 2px; width:100%; height:36px; padding-top: 8px;">
    <a href="https://www.iubenda.com/privacy-policy/74934126" class="iubenda-black iubenda-embed " title="Privacy Policy">Privacy Policy</a> 
    <script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+   
+   <a href="https://www.iubenda.com/privacy-policy/74934126/cookie-policy" class="iubenda-black iubenda-embed " title="Cookie Policy">Cookie Policy</a> 
+   <script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script> 
 </div>
