c724e6c3e1589e5ccc08c862f9fd841b663f5f9b
marco.casu <marco.casu@NEWLIFE>
2018-05-22 17:06:54 +0200

    Aggiunto footer con privacy policy.

--- include/footer.php
+++ include/footer.php
@@ -18,7 +18,7 @@
  */
 ?>
 
-<div class="text-center" style="background-color: #3c78c3; position:relative; bottom:0; border-right: 1px; border-left: 1px; width:100%; height:36px; padding-top: 8px;">
+<div class="text-center" style="background-color: #3c78c3; position:relative; bottom:0; border-right: 2px; border-left: 2px; width:100%; height:36px; padding-top: 8px;">
    <a href="https://www.iubenda.com/privacy-policy/74934126" class="iubenda-black iubenda-embed " title="Privacy Policy">Privacy Policy</a> 
    <script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
 </div>
