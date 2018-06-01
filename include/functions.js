2c37b16a966183f3bf4ec71f1e3734574002b9af
marco.casu <marco.casu@NEWLIFE>
2018-06-01 18:16:25 +0200

    Migliorato gestione utenti.

--- include/functions.js
+++ include/functions.js
@@ -289,7 +289,7 @@ StreamVideoSizeUpdate = function()
    
 CheckIfUserSelectedIsMine = function(userSelectedId, groupCurrentId) 
 {
-    console.log("User id: " + userSelectedId);
+    console.log("User id: " + userSelectedId + " Group current id: " + groupCurrentId);
     var params = 'fname=check_if_user_selected_is_mine&userSelectedId=' + userSelectedId + '&groupCurrentId=' + groupCurrentId;
     var result;
     jQuery.ajax({
